1c2b920a9bd618d94ac2af6b623d4eca40789789
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 23:37:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,3 +3,17 @@ def roll_call_dwarves (names)
     puts "#{position+1}. #{dwarf}"
   end
 end
+
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call.capitalize + "!"
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
